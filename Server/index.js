d9a6f4338a2956528323639f33b8d4b43c39732e
ChezNoodles123 <subbu20204@gmail.com>
2024-08-07 21:42:13 +0530

    Update index.js, added Versel frontend URL

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-      origin: ["https://deploy-mern-frontend.vercel.app"],
+      origin: ["https://to-do-list-frontend-one.vercel.app/"],
       methods: ["POST", "GET"],
       credentials: true
   }
